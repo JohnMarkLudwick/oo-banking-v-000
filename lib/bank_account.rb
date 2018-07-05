74d06997884c5d2d11ee634a439c7935f8393306
JohnMark <johnmarkludwick@gmail.com>
2018-07-05 12:24:12 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,22 @@
 class BankAccount
 
+attr_accessor :balance, :status
+attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(money)
+    @balance =+ 1000
+    
+  end
+  
+  
+  
+
+
+
 end
