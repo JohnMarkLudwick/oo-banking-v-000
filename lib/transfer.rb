0ef1a67b9f8a5c18e9bf03ef4717e28682fc4b51
JohnMark <johnmarkludwick@gmail.com>
2018-07-05 17:18:48 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
     if valid? && @receiver.balance > @amount
       @receiver.balance -= @amount
       @sender.balance += @amount
-      @status = "reversed"
+      @status = "complete"
       
     else
     end
